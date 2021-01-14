fdd1ff634fe175e1444cf9fae3810e108407d56c
nuraanNinah <nuraanbarnes26@gmail.com>
2021-01-14 07:46:50 +0200

    first commit

---
+++ index.py
@@ -0,0 +1,24 @@
+
+
+x = 0
+y = 1
+print(x)
+print(y)
+
+
+x = x + 3
+y = y + x
+print(x)
+print(y
+
+x = 1 + 1 * 2
+y = (1 + 1) * 2
+z = 1 + ( 1 * 2 )
+a = 1 + 1 * 2 / 2
+b = (1 + 1 * 2 ) /  2
+
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
