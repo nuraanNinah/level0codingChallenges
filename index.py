c8174efbe54c0a70700802c02b64c4fa52ab183d
nuraanNinah <nuraanbarnes26@gmail.com>
2021-02-04 07:52:35 +0200

    fixing errors

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-#Task 0.1
+#Task 0.1 
 x = 0
 y = 1
 print(x)
@@ -24,8 +24,9 @@ print(a)
 print(b)
 
 #Task 0.3 function named hello
-name = 'Hello Tshepo'
-print(name)
+def hello():
+    print("Hello Tshepo!")
+hello()
 
 #Task 0.4  function named even_or_odd
 def evenOrOdd(num):
@@ -34,23 +35,19 @@ def evenOrOdd(num):
     else:
         print(num, "is Odd")
 
-evenOrOdd(26)
-evenOrOdd(25)
 
-#Task 0.5 function of a triangle
-a = 3
-b = 4
-c = 6
-s = (a + b + c) / 2
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print(area)
+#Task 0.5 function of a triangle 
+def Triangle(s1, s2, s3):
+	area = (2 * 3 * 2)/2
+	return area
 
-#Task 0.6
+#Task 0.6   Task 0.6 - it is completely wrong. You are required to CREATE A FUNCTION that takes in 3 numbers (which you will have to modify to accommodate any set of numbers) and returns the biggest number out of them.
+def maximum()
 x = max(1,2,3)
 
 print(x)
 
-#Task 0.7
+#Task 0.7    Task 0.7 - write two functions, to convert Fahrenheit to Celsius and another on to convert Celsius to Fahrenheit.
 celsius = 35.7
 
 fahrenheit = (celsius * 1.8) + 32
@@ -63,13 +60,13 @@ def convert(n):
 n = 71
 print(convert(n))
 
-#Task 0.9 print out vowels
+#Task 0.9 print out vowels   Task 0.9 - missing a function 
 v = "fat cat sat on the mat"
 for i in v:
     if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
     print(i)
 
-#Task 0.10 common letters
+#Task 0.10 common letters   Task 0.10 - missing a function.
 string1=("house")
 string2=("computers")
 a=list(set(sstring1)&set(sstring2))
